78446ba550c790cb5d63030f1be07a170b05c74a
Antony Woods <antony@mastodonc.com>
2016-10-17 14:48:23 +0100

    Version 0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kixi/kixi.comms "0.1.2"
+(defproject kixi/kixi.comms "0.1.3-SNAPSHOT"
   :description "FIXME: write description"
   :url "https://github.com/MastodonC/kixi.comms"
   :license {:name "Eclipse Public License"
