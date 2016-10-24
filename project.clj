c281682d69420f178a4d9e8108644cd9f4ef4933
Tom Coupland <tcoupland@gmail.com>
2016-10-24 14:22:29 +0100

    Version 0.1.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (def slf4j-version "1.7.21")
-(defproject kixi/kixi.comms "0.1.6"
+(defproject kixi/kixi.comms "0.1.7-SNAPSHOT"
   :description "FIXME: write description"
   :url "https://github.com/MastodonC/kixi.comms"
   :license {:name "Eclipse Public License"
