815759b25c71408e97442fc58e786c278909f146
Antony Woods <antony@mastodonc.com>
2016-10-31 13:17:10 +0000

    Version 0.1.14-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (def slf4j-version "1.7.21")
-(defproject kixi/kixi.comms "0.1.13"
+(defproject kixi/kixi.comms "0.1.14-SNAPSHOT"
   :description "FIXME: write description"
   :url "https://github.com/MastodonC/kixi.comms"
   :license {:name "Eclipse Public License"
